4a08a6475adce3595d644371c34ed9a2a2c9164c
woutvos <mail@woutvos.nl>
2022-09-28 20:11:41 +0200

    Add functions to change group things

--- hue.py
+++ hue.py
@@ -19,6 +19,8 @@ DB_PATH = 'settings.db'
 logging.info(f'Using: {DB_PATH}')
 
 
+# Bridge things
+
 def connect_to_latest_bridge():
     """Connect to the latest used bridge"""
     # Connect to database
@@ -96,6 +98,8 @@ def get_bridges():
     return bridges
 
 
+# Light things
+
 def get_lights(b):
     """Get all the lights"""	
     if b == 'example':
@@ -132,6 +136,38 @@ def get_lights(b):
     return lights_list
 
 
+def get_light(b, light_id):
+    """Get the light info for a specific light"""
+    if b == 'example':
+        logging.info('Using example data for light')
+        light = examples.light_example
+
+    else:
+        # Get the light
+        light_raw = b.get_light(light_id)
+
+        # Get the light info
+        light = {
+            'id': light_id,
+            'name': light_raw['name'],
+            'status': light_raw['state']['on'],
+            'brightness': light_raw['state']['bri'],
+            'colormode': light_raw['state']['colormode'],
+            'hue': light_raw['state']['hue'],
+            'saturation': light_raw['state']['sat'],
+            'xy': light_raw['state']['xy'],
+            'hex': converter.xy_to_hex(light_raw['state']['xy'][0], light_raw['state']['xy'][1]),
+            'colortemp': light_raw['state']['ct'],
+            'effect': light_raw['state']['effect'],
+            'alert': light_raw['state']['alert'],
+            'reachable': light_raw['state']['reachable'],
+            'type': light_raw['type']
+        }
+
+        logging.info(f'Found light: {light}')
+    return light
+
+
 def light_on_off(b, light_id, state):
     """Turn light on and off"""
     if state == 'on':
@@ -161,13 +197,27 @@ def set_light_color(b, light_id, color):
     logging.info(f'Light {light_id} color set to {color}')
 
 
-def get_light_names(light_id, lights):
+# Things needed for the group functions to work
+
+def light_names(light_id, lights):
     """Get the id's of the lights and their corresponding names"""
     # Get the name of the light
     for l in lights:
         if l['id'] == light_id:
             return l['name']
 
+
+def average_brightness(b, lights):
+    """Get the average brightness of the lights"""
+    brightness = 0
+    for l in lights:
+        brightness = brightness + get_light(b, int(l))['brightness']
+
+    average_brightness = brightness / len(lights)
+    return int(average_brightness)
+
+
+# Group things
 
 def get_groups(b):
     """Get all the groups"""
@@ -191,10 +241,11 @@ def get_groups(b):
                 'name': groups_raw[g]['name'],
                 'lights': {
                     'id': groups_raw[g]['lights'],
-                    'name': [get_light_names(l, lights) for l in [eval(i) for i in groups_raw[g]['lights']]]
+                    'name': [light_names(l, lights) for l in [eval(i) for i in groups_raw[g]['lights']]]
                 },
                 'type': groups_raw[g]['type'],
                 'state': groups_raw[g]['state'],
+                'average_brightness': average_brightness(b, groups_raw[g]['lights'])
             },
         )
 
@@ -212,3 +263,21 @@ def group_on_off(b, group_id, state):
     elif state == 'off':
         b.set_group(group_id, 'on', False)
         logging.info(f'Group {group_id} turned off')
+
+
+def set_group_brightness(b, group_id, brightness):
+    """Set the brightness of the group"""
+    b.set_group(group_id, 'bri', brightness)
+    logging.info(f'Group {group_id} brightness set to {brightness}')
+
+
+def set_group_color(b, group_id, color):
+    """Set the color of the group"""
+    # Convert the color to xy
+    color = color.lstrip('#')
+    xy = converter.hex_to_xy(color)
+    logging.debug(f'Color {color} converted to xy: {xy}')
+
+    # Set the color
+    b.set_group(group_id, 'xy', xy)
+    logging.info(f'Group {group_id} color set to {color}')
